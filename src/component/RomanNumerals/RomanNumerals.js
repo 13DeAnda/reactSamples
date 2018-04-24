9e50d22c58635238bebc3e53593e61477444e5d9
nora <nora.e.de.anda@gmail.com>
2018-04-23 19:30:22 -0700

    rm - converting number to roman

--- src/component/RomanNumerals/RomanNumerals.js
+++ src/component/RomanNumerals/RomanNumerals.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './RomanNumerals.css';
+import _ from 'lodash';
 import { Grid } from 'react-bootstrap';
 class RomanNumerals extends React.Component {
 
@@ -10,11 +11,11 @@ class RomanNumerals extends React.Component {
       romanNumbers: {
         M: {number: 1000, extractor: {roman: "C", number: 100}, isExtractor: false},
         D: {number: 500, extractor:{roman: "C", number: 100}, isExtractor: false},
-        C: {number: 100, extractor:{roman: "X", number: 10}, isExtractor: true},
+        C: {number: 100, extractor:{roman: "X", number: 10}, isExtractor: true, grouped: "D"},
         L: {number: 50, extractor:{roman: "X", number: 10}, isExtractor: false},
-        X: {number: 10, extractor:{roman: "I", number: 1}, isExtractor: true},
+        X: {number: 10, extractor:{roman: "I", number: 1}, isExtractor: true, grouped:"L"},
         V: {number: 5, extractor:{roman: "I", number: 1}, isExtractor: false},
-        I: {number: 1, extractor: null, isExtractor: true}
+        I: {number: 1, extractor: null, isExtractor: true, grouped: "V"}
       }
     };
   }
@@ -46,10 +47,34 @@ class RomanNumerals extends React.Component {
     console.log("the final", finalConvertedNumber);
   }
 
+  numberToRoman = (valueToConvert) =>{
+    this.setState({started: true});
+    console.log(valueToConvert);
+    var residue = valueToConvert;
+    var romanNumber = "";
+    _.forEach(this.state.romanNumbers, function(item, key){
+      if(residue % item.number < residue){
+        var repeated = Math.floor(residue/item.number);
+        residue = residue % item.number;
+        console.log(key, residue, repeated);
+        if(repeated < 4){
+          for(var i =0; i < repeated; i++){
+            romanNumber += key;
+          }
+        }
+        else{
+          romanNumber += key + item.grouped;
+        }
+        
+      }
+    });
+    console.log("the romanNumber", romanNumber);
+  }
+
   render() {
     return (
       <Grid className="container-fluid RomanNumerals">
-        {!this.state.started? this.romanToNumber('CII') : null}
+        {!this.state.started? this.numberToRoman(1666) : null}
       </Grid>
     );
   }

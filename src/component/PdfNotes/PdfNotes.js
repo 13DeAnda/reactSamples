2177ab945c278e0fe116582a121064d9006a0d76
nora <nora.e.de.anda@gmail.com>
2018-05-04 13:16:52 -0700

    pdf - basic setup

--- src/component/PdfNotes/PdfNotes.js
+++ src/component/PdfNotes/PdfNotes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
 import { Document, Page } from 'react-pdf';
 import './PdfNotes.css';
+import '../../shared/shared.css';
 import _ from 'lodash';
 
 
@@ -10,43 +11,59 @@ export default class PdfNotes extends React.Component {
   constructor(props) {
     super(props);
     this.onChange = this.onChange.bind(this);
-    this.onLoadSuccess = this.onLoadSuccess.bind(this);
+    this.previousPage = this.previousPage.bind(this);
+    this.nextPage = this.nextPage.bind(this);
     this.state = {
       pdfPath: "",
-      numPages: 4,
+      numPages: null,
       pageNumber: 2
     };
+  }
+  onDocumentLoad = (res) => {
+    this.setState({numPages: res.numPages});
   }
 
   render() {
     return (
       <Grid className="container-fluid RomanNumerals">
-        <Row className="text-center">
-          <Col>
-            this will be the notes where it takes it
+        <Row>
+          <Col sm={4}>
+            this is the menu
           </Col>
-          <Col>
-            <Document
-              file="relativity.pdf"
-              onLoadSuccess={this.onDocumentLoad}
-            >
-              <Page pageNumber={this.state.pageNumber} />
-            </Document>
-            <p>Page {this.state.pageNumber} of {this.state.numPages}</p>
+          <Col sm={6}>
+            <Row className='text-center'>
+              <Col xsOffset={4} sm={2} md={2}> <button onClick={this.previousPage}> previous </button> </Col>
+              <Col sm={3} md={3} className='text-center'> Page {this.state.pageNumber} of {this.state.numPages} </Col>
+              <Col sm={2} md={2}> <button onClick={this.nextPage}> next </button> </Col>
+            </Row>
+            <Row>
+              <Document
+                file="relativity.pdf"
+                onLoadSuccess={this.onDocumentLoad}>
+                <Page pageNumber={this.state.pageNumber} />
+              </Document>
+            </Row>
           </Col>
-
         </Row>
       </Grid>
     );
+  }
+
+  nextPage(){
+    if(this.state.pageNumber < this.state.numPages){
+      this.setState({pageNumber: ++this.state.pageNumber});
+    }
+  }
+
+  previousPage(){
+    if(this.state.pageNumber > 1){
+      this.setState({pageNumber: --this.state.pageNumber});
+    }
   }
 
   onChange (e){
     this.setState({textToConvert : e.target.value, message: null}); 
   }
 
-  onLoadSuccess(value){
-    this.setState({ numPages: value });
-    console.log("value", value);
-  }
 
 }

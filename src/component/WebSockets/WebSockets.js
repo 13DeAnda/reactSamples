b545d631e874b9db9e8c1b82492b65d0a592fda7
nora <nora.e.de.anda@gmail.com>
2018-06-17 22:16:17 -0500

    server- basic setup for web sockets

---
+++ src/component/WebSockets/WebSockets.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
+import _ from 'lodash';
+
+
+
+export default class WebSockets extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+
+  render() {
+    return (
+      <div>
+        this is the WebSockets sample
+      </div>
+    );
+  }
+
+
+}

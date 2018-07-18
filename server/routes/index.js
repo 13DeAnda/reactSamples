0a8a95e75017c03975af5a0ccf09ca68cd599b5c
nora <nora.e.de.anda@gmail.com>
2018-07-17 19:05:53 -0500

    chat - api get users

--- server/routes/index.js
+++ server/routes/index.js
@@ -84,4 +84,28 @@ router.post('/api/users', (req, res, next) => {
   });
 });
 
+router.get('/api/users', (req, res, next) => {
+  const results = [];
+  // Get a Postgres client from the connection pool
+  pg.connect(connectionString, (err, client, done) => {
+    // Handle connection errors
+    if(err) {
+      done();
+      console.log(err);
+      return res.status(500).json({success: false, data: err});
+    }
+    // SQL Query > Select Data
+    const query = client.query('SELECT * FROM users ORDER BY id ASC;');
+    // Stream results back one row at a time
+    query.on('row', (row) => {
+      results.push(row);
+    });
+    // After all data is returned, close connection and return results
+    query.on('end', () => {
+      done();
+      return res.json(results);
+    });
+  });
+});
+
 module.exports = router;
